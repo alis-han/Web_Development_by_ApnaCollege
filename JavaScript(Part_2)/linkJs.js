aac9a8b26fc08e5d6aba2494e39341fd845cd4c6
alis-han <114800068+alis-han@users.noreply.github.com>
2023-07-12 08:02:04 +0530

    add

---
+++ JavaScript(Part_2)/linkJs.js
@@ -0,0 +1,17 @@
+console.log("Alishan");
+let a=5;
+let b=7;
+console.log("Sum is: ",a+b);
+
+// TEMPLATE_LITERALS
+let pencilPrice=10;
+let erasorPrice=5;
+let output="The sum is: "+(pencilPrice+erasorPrice)+" rupees";
+console.log(output);
+
+console.log(`The sums are: $(pencilPrice + erasorPrice) rupees.`);
+
+// Alert & Prompt
+alert("heyy...");
+let name=prompt("Enter your name: ");
+console.log(name);
